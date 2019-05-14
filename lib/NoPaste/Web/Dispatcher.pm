073d454b09e7fa7fc57feaa0243f39483f1e5ec5
maeshiro <maeshiro@mfac.jp>
2019-05-14 13:49:39 +0900

    step1

--- lib/NoPaste/Web/Dispatcher.pm
+++ lib/NoPaste/Web/Dispatcher.pm
@@ -4,13 +4,12 @@ use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
 
-any '/' => sub {
-    my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
+get '/' => sub {
+    my ($c, $args) = @_;
+
+    return $c->render('form.tx', {
+        action => '/',
+        button => 'create',
     });
 };
 
